92f69be8642a6828044211c8b467980b8f51993a
nutpakorn-cat <nutpakorn.tas@student.mahidol.edu>
2021-05-20 09:44:03 +0700

    :tada: recolor navbar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="footer">
-        <div data-v-32a80de1="" class="" style="width: 100%;height: 100px;position: fixed;background-color: white;bottom: -32px;"></div>
+        <div data-v-32a80de1="" class="" style="width: 100%;height: 100px;position: fixed;background-color: white;bottom: -47px;"></div>
         <div class="row">
-            <div class="col-md-4 text-left">
+            <div class="col-md-4 text-left" style="position: relative;top: 6px;">
                 <a :href="footerData.facebook"><img :src="require('@/assets/PNG/AllPage_Facebook_ICON.png')" width="130"></a>
             </div>
             <div class="col-md-4">
@@ -10,7 +10,7 @@
                     <div class="footer-image" :style="{backgroundImage: 'url(' + require('@/assets/PNG/Welcome_Let_sGoButton.png') + ')'}"></div>
                 </router-link>
             </div>
-            <div class="col-md-4 text-right">
+            <div class="col-md-4 text-right" style="position: relative;top: 6px;">
                 <p style="color: black;line-height: 142px;margin-right: 53px;">{{footerData.copyright}}</p>
             </div>
         </div>
@@ -47,7 +47,13 @@ export default {
 
 @media (min-width: 768px) {
     .footer-image {
-        height: 90px;width: 200px;margin-left: auto;margin-right: auto;background-size: 153%;background-position-x: center;background-position-y: -113px;
+        height: 90px;
+        width: 200px;
+        margin-left: auto;
+        margin-right: auto;
+        background-size: 123%;
+        background-position-x: center;
+        background-position-y: -89px;
         margin-top: -53px;
     }
 }
